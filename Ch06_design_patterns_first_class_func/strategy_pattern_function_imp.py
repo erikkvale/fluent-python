efabaf8279a60e5abb74fcef99f3f881914161cb
Erik <eirikval@gmail.com>
2018-03-09 15:44:21 -0800

    Started new module for function-oriented implementation of strategy

---
+++ Ch06_design_patterns_first_class_func/strategy_pattern_function_imp.py
@@ -0,0 +1,60 @@
+"""
+This is the function-oriented implementation of the strategy design pattern
+-Context
+
+- Functions not ABC and concrete classes
+    -Strategy (interface)
+    -Concrete strategies
+
+"""
+
+from collections import namedtuple
+
+Customer = namedtuple('Customer', 'name fidelity')
+
+
+class LineItem:
+
+    def __init__(self, product, quantity, price):
+        self.product = product
+        self.quantity = quantity
+        self.price = price
+
+    def total(self):
+        return self.price * self.quantity
+
+
+class Order:
+    """
+    Context
+    """
+    def __init__(self, customer, cart, promotion=None):
+        self.customer = customer
+        self.cart = list(cart)
+        self.promotion = promotion
+
+    def total(self):
+        if not hasattr(self, '__total'):
+            self.__total = sum(item.total() for item in self.cart)
+        return self.__total
+
+    def due(self):
+        if self.promotion is None:
+            discount = 0
+        else:
+            discount = self.promotion.discount(self)
+        return self.total() - discount
+
+    def __repr__(self):
+        fmt = '<Order total: {:.2f} due: {:.2f}>'
+        return fmt.format(self.total(), self.due())
+
+
+def fidelity_item_promo(order):
+    """
+
+    :param order:
+    :type order:
+    :return:
+    :rtype:
+    """
