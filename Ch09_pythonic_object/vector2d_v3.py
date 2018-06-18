02059b1e186d1c3f880f2b1757974442471bd1a7
Erik Kvale <eirikval@gmail.com>
2018-06-18 14:23:47 -0500

    Culmination of all prior versions of class, working on doctests

--- Ch09_pythonic_object/vector2d_v3.py
+++ Ch09_pythonic_object/vector2d_v3.py
@@ -1,62 +1,103 @@
 """
-A two dimensional vector class
+A two-dimensional vector class
 
-    >>> v1 = Vector2d(3, 4)
-    >>> print(v1.x, v1.y)
-    3.0 4.0
-    >>> x, y = v1
-    >>> x, y
-    (3.0, 4.0)
-    >>> v1
-    Vector2d(3.0, 4.0)
-    >>> v1_clone = eval(repr(v1))
-    >>> v1 == v1_clone
-    True
-    >>> print(v1)
-    (3.0, 4.0)
-    >>> octets = bytes(v1)
-    >>> octets
-    b'd\\x00\\x00\\x00\\x00\\x00\\x00\\x08@\\x00\\x00\\x00\\x00\\x00\\x00\\x10@'
-    >>> abs(v1)
-    5.0
-    >>> bool(v1), bool(Vector2d(0, 0))
-    (True, False)
+>>> v1 = Vector2d(3, 4)
+>>> print(v1.x, v1.y)
+3.0 4.0
+>>> x, y = v1
+>>> x, y
+(3.0, 4.0)
+>>> v1
+Vector2d(3.0, 4.0)
+>>> v1_clone = eval(repr(v1))
+>>> v1 == v1_clone
+True
+>>> print(v1)
+(3.0, 4.0)
+>>> octets = bytes(v1)
+>>> octets
+b'd\\x00\\x00\\x00\\x00\\x00\\x00\\x08@\\x00\\x00\\x00\\x00\\x00\\x00\\x10@'
+>>> abs(v1)
+5.0
+>>> bool(v1), bool(Vector2d(0, 0))
+(True, False)
 
-
+Test of ``.from_bytes()`` class method:
+>>> v1_clone = Vector2d.from_bytes(bytes(v1))
+>>> v1_clone
+Vector2d(3.0, 4.0)
+>>> v1 == v1_clone
+True
 """
 from array import array
 import math
 
 
 class Vector2d:
-
-    type_code = 'd'
+    TYPE_CODE = 'd'
 
     def __init__(self, x, y):
-        self.x = float(x)
-        self.y = float(y)
+        self.__x = float(x)
+        self.__y = float(y)
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
 
     def __iter__(self):
         return (i for i in (self.x, self.y))
 
-    def __repr__(self):
-        class_name = type(self).__name__
-        return '{}({!r}, {!r})'.format(class_name, *self)
-
     def __str__(self):
         return str(tuple(self))
 
-    def __eq__(self, other):
-        return tuple(self) == tuple(other)
+    def __repr__(self):
+        return "{}({}, {})".format(
+            type(self).__name__,
+            *self
+        )
 
     def __bytes__(self):
         return (
-            bytes([ord(self.type_code)]) +
-            bytes(array(self.type_code, self))
+                bytes([ord(self.TYPE_CODE)]) +
+                bytes(array(self.TYPE_CODE, self))
         )
+
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
 
     def __abs__(self):
         return math.hypot(self.x, self.y)
 
     def __bool__(self):
         return bool(abs(self))
+
+    def __format__(self, format_spec=''):
+        if format_spec.endswith('p'):
+            format_spec = format_spec[:-1]
+            coords = (abs(self), self.angle())
+            outer_fmt = '<{}, {}>'
+        else:
+            coords = self
+            outer_fmt = '({}, {})'
+        components = (format(c, format_spec) for c in coords)
+        return outer_fmt.format(*components)
+
+    def __hash__(self):
+        return hash(self.x) ^ hash(self.y)
+
+    def angle(self):
+        return math.atan2(self.x, self.y)
+
+    @classmethod
+    def from_bytes(cls, octets):
+        type_code = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(type_code)
+        return cls(*memv)
+
+if __name__=='__main__':
+    import doctest
+    doctest.testmod()
