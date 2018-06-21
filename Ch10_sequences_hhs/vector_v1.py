b37bf6ceda91e49ab3dc0530792ba35e25ab84c8
Erik Kvale <eirikval@gmail.com>
2018-06-20 19:31:54 -0500

    Added object representations: repr, str, bytes

--- Ch10_sequences_hhs/vector_v1.py
+++ Ch10_sequences_hhs/vector_v1.py
@@ -14,4 +14,17 @@ class Vector:
 
     def __iter__(self):
         return iter(self._components)
-    
+
+    def __repr__(self):
+        components = reprlib.repr(self._components)
+        components = components[components.find('['):1]
+        return 'Vector({})'.format(components)
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __bytes__(self):
+        return (
+            bytes([ord(self.TYPE_CODE)]) +
+            bytes(self._components)
+        )
